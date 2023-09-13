adafab1d2f86b6458c22c8bdd4028f9a9454706e
Raj Kumar <raj.kumar@litmus7.com>
2023-09-13 20:24:11 +0530

    Adding test case

--- src/components/__test__/Blog.test.tsx
+++ src/components/__test__/Blog.test.tsx
@@ -4,8 +4,8 @@ import "@testing-library/dom";
 import "@testing-library/jest-dom/extend-expect";
 import Blog from "../Blog";
 
-describe("<InputBox />", () => {
-  it("render Input box with both input element and label element", async () => {
+describe("<Blog />", () => {
+  it("render Blog component as expected", async () => {
     const setVotesMock = jest.fn();
     const { queryByText } = render(
       <Blog
@@ -26,6 +26,27 @@ describe("<InputBox />", () => {
     );
     expect(queryByText("Getting started")).toBeTruthy();
     expect(queryByText("Jane Doe")).toBeTruthy();
+  });
+
+  it("clicking upVote should invoke the expected method", async () => {
+    const setVotesMock = jest.fn();
+    render(
+      <Blog
+        blogData={[
+          {
+            id: 2,
+            title: "Getting started",
+            votes: 97,
+            cover:
+              "https://nextjs.org/static/images/learn/foundations/components.png",
+            author: "Jane Doe",
+          },
+        ]}
+        superProp="Hi-there"
+        votes={10}
+        setVotes={setVotesMock}
+      />
+    );
     await waitFor(() => userEvent.click(screen.getAllByTestId("upvote")[0]));
     expect(setVotesMock).toHaveBeenCalled();
   });
