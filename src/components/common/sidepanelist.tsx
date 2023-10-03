b3b48cea8ee8deaa3f6481ff209311aec1e9445b
Thasnim <Thasnim@litmus7.com>
2023-10-03 12:04:58 +0530

    Sidepane list style changes

--- src/components/common/sidepanelist.tsx
+++ src/components/common/sidepanelist.tsx
@@ -13,9 +13,12 @@ const SidePaneList: FC<Props> = ({ menuList, selectedMenu, setSelectedMenu }) =>
         <div className='hidden sm:block w-80 border-slate-200 shadow-slate-200 border-solid border-r min-h-[calc(100vh-4.6rem)]'>
             <div>
                 {menuList.map(menu =>
-                    <span key={menu.id} className={`block py-4 pl-10 pr-2 text-neutral-500 text-sm  cursor-pointer ${(selectedMenu === menu.id) ? 'font-semibold' : 'font-normal'}`} onClick={() => setSelectedMenu(menu.id)}>
+                    <div key={menu.id} className={`flex pl-10 pr-2 py-4 mx-4 ${(selectedMenu === menu.id) ? 'bg-gray-200 rounded-lg' : 'bg-transparent'}`} onClick={() => setSelectedMenu(menu.id)}>
+                        <img src={menu.icon} alt="menu.name" />
+                        <span className={`block pl-6 text-neutral-500 text-sm  cursor-pointer ${(selectedMenu === menu.id) ? 'font-semibold' : 'font-normal'}`}>
                         {menu.name}
                     </span>
+                    </div>
                 )
                 }
             </div>
