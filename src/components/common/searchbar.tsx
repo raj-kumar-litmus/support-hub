31226da3226136dc2e189485eb5d8eb7508e20c1
Thasnim <Thasnim@litmus7.com>
2023-10-04 14:21:58 +0530

    Reusable input text component and its storybook

--- src/components/common/searchbar.tsx
+++ src/components/common/searchbar.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 import SearchIcon from "../../assets/search.svg";
-import { InputText } from "primereact/inputtext";
 import { Button } from 'primereact/button';
 import CustomImage from './customimage';
+import CustomInputText from './custominputtext';
 
 type Props = {
     showSearchButton: boolean;
@@ -16,7 +16,7 @@ const SearchBar: FC<Props> = ({ showSearchButton, setOpenSearchField, openSearch
 
     return (
         <div className="block">
-            <InputText type="text" name="q" className="w-full min-w-[400px] h-10 bg-gray-100  p-4 rounded-full hidden sm:block focus:outline-none placeholder:text-stone-500 placeholder:font-helvetica placeholder:font-medium" placeholder="Search Order #" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+            <CustomInputText type="text" className="w-full min-w-[400px] h-10 bg-gray-100  p-4 rounded-full hidden sm:block focus:outline-none placeholder:text-stone-500 placeholder:font-helvetica placeholder:font-medium" placeholder="Search Order #" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
             {showSearchButton &&
                 <Button type="submit" className='-ml-8 sm:hidden' onClick={() => setOpenSearchField(!openSearchField)}>
                     <CustomImage className="h-[13px]" src={SearchIcon} alt="Search" />
--- src/components/common/searchfield.tsx
+++ src/components/common/searchfield.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
 import RightArrowIcon from "../../assets/right_arrow.svg";
-import { InputText } from "primereact/inputtext";
 import { Button } from 'primereact/button';
 import CustomImage from "./customimage";
+import CustomInputText from "./custominputtext";
 
 type Props = {
     searchValue: string;
@@ -13,7 +13,7 @@ const SearchField: FC<Props> = ({ searchValue, setSearchValue }) => {
 
     return (
         <div className="flex sm:hidden px-6 py-3 border-slate-200  border-solid	 border-b justify-between">
-            <InputText type="text" name="q" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="w-full focus:outline-none placeholder:text-stone-500 bg-transparent placeholder:font-helvetica placeholder:font-medium" placeholder="Search Order" />
+            <CustomInputText type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="w-full focus:outline-none placeholder:text-stone-500 bg-transparent placeholder:font-helvetica placeholder:font-medium" placeholder="Search Order" />
             <Button type="submit" className='-ml-8'>
                 <CustomImage className="arrow-icon" src={RightArrowIcon} alt="Search" />
             </Button>
