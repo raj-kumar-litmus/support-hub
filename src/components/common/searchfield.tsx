a13c7dc47091d03f333cd98a3aec5c1fc3b2bc03
Thasnim <Thasnim@litmus7.com>
2023-10-05 16:37:06 +0530

    icons and input text focus highlight style fix

--- src/components/common/searchfield.tsx
+++ src/components/common/searchfield.tsx
@@ -10,16 +10,11 @@ type Props = {
 };
 
 const SearchField: FC<Props> = ({ searchValue, setSearchValue }) => {
+
   return (
-    <div className="flex sm:hidden px-6 py-3 border-slate-200  border-solid	 border-b justify-between">
-      <CustomInputText
-        type="text"
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
-        className="w-full focus:outline-none placeholder:text-stone-500 bg-transparent placeholder:font-helvetica placeholder:font-medium"
-        placeholder="Search Order"
-      />
-      <Button type="submit" className="-ml-8">
+    <div className="flex sm:hidden px-6 border-slate-200  border-solid	 border-b justify-between">
+      <CustomInputText type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="w-full focus:outline-none placeholder:text-stone-500 bg-transparent placeholder:font-helvetica placeholder:font-medium !shadow-none !border-none" placeholder="Search Order" />
+      <Button text className='-ml-8 button-focus !shadow-none'>
         <CustomImage className="arrow-icon" src={RightArrowIcon} alt="Search" />
       </Button>
     </div>
