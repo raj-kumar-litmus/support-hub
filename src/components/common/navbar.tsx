312350aa920af15551e48b6851790f349ef5c05c
Thasnim <Thasnim@litmus7.com>
2023-10-05 16:37:27 +0530

    Navbar height made fixed

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -20,25 +20,13 @@ const Navbar: FC<Props> = ({
   const [searchValue, setSearchValue] = useState<string>("");
 
   return (
-    <div className="py-4 pl-4 pr-1 sm:px-10 flex items-center border-slate-200  border-solid	 border-b justify-between">
-      <div className="flex">
-        <CustomImage
-          className="h-[13px] sm:hidden pr-4 cursor-pointer"
-          src={MenuIcon}
-          alt="Menu"
-          onClick={() => setShowSidePaneGrid(!showSidePaneGrid)}
-        />
-        <CustomImage
-          className="h-[13px] cursor-pointer"
-          src={SephoraLogo}
-          alt="SEPHORA"
-        />
+    <div className='py-4 pl-4 pr-1 sm:px-10 flex items-center border-slate-200  border-solid	 border-b justify-between h-16'>
+      <div className='flex'>
+        <CustomImage className="h-[13px] sm:hidden pr-4 cursor-pointer" src={MenuIcon} alt="Menu" onClick={() => setShowSidePaneGrid(!showSidePaneGrid)} />
+        <CustomImage className="h-[13px] cursor-pointer" src={SephoraLogo} alt="SEPHORA" />
       </div>
-      <div className="sm:m-auto">
-        <SearchBar
-          showSearchButton={!showSidePaneGrid}
-          setOpenSearchField={setOpenSearchField}
-          openSearchField={openSearchField}
+      <div className='sm:m-auto'>
+        <SearchBar showSearchButton={!showSidePaneGrid} setOpenSearchField={setOpenSearchField} openSearchField={openSearchField}
           searchValue={searchValue}
           setSearchValue={setSearchValue}
         />
