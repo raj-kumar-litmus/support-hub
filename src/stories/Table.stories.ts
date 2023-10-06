aa8359a6b92daa14f4e438f3eddff7aedc59cf04
Adithya <adithya@litmus7.com>
2023-10-06 18:04:40 +0530

    table storybook

--- src/stories/Table.stories.ts
+++ src/stories/Table.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta } from "@storybook/react";
-import Table from "../components/common/Table";
+import Table from "./Table";
 
 const meta = {
   title: "Example/Table",
@@ -12,27 +12,64 @@ const meta = {
 
 export default meta;
 
+const tableData = [
+  {
+    "DC Name": "UDC",
+    Country: "US",
+    "Shipment Node": "0801",
+    "Workable Orders": 3562,
+  },
+  {
+    "DC Name": "SDC",
+    Country: "US",
+    "Shipment Node": "1001",
+    "Workable Orders": 18451,
+  },
+  {
+    "DC Name": "WDC",
+    Country: "US",
+    "Shipment Node": "1021",
+    "Workable Orders": 16620,
+  },
+];
+
 export const Default = {
   args: {
-    tableData: [
-      {
-        "DC Name": "UDC",
-        Country: "US",
-        "Shipment Node": "0801",
-        "Workable Orders": 3562,
-      },
-      {
-        "DC Name": "SDC",
-        Country: "US",
-        "Shipment Node": "1001",
-        "Workable Orders": 18451,
-      },
-      {
-        "DC Name": "WDC",
-        Country: "US",
-        "Shipment Node": "1021",
-        "Workable Orders": 16620,
-      },
-    ],
+    tableData: tableData,
+  },
+};
+
+export const StripedRows = {
+  args: {
+    tableData: tableData,
+    stripedRows: true,
+  },
+};
+
+export const TableGrid = {
+  args: {
+    tableData: tableData,
+    showGridlines: true,
+  },
+};
+
+export const RadioSelection = {
+  args: {
+    tableData: tableData,
+    selectionMode: "radio",
+  },
+};
+
+export const CheckboxSelection = {
+  args: {
+    tableData: tableData,
+    selectionMode: "checkbox",
+  },
+};
+
+export const SortableColumns = {
+  args: {
+    tableData: tableData,
+    sortable: true,
   },
 };
