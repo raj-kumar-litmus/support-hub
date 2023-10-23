4a2bbae2360cdf761a71f0791867ac3f81b22dc1
sarath-sasi-panickaveetil <sarath.sasi@litmus7.com>
2023-10-23 13:46:45 +0530

    PR-14 Review comments addressed

--- src/components/orderDetails/OrderDetails.tsx
+++ src/components/orderDetails/OrderDetails.tsx
@@ -142,7 +142,7 @@ const OrderDetails: React.FC = () => {
     <Loader />
   ) : Object.keys(orderData).length > 0 ? (
     <div id="orderDetailsComp">
-      <div className="grid gap-y-0 grid-cols-1 m-4 p-0 sm:py-4 sm:px-6 sm:bg-[#30343B] rounded-md">
+      <div className="gridNoGapRounded grid-cols-1 m-4 p-0 sm:py-4 sm:px-6 sm:bg-[#30343B] ">
         <div className="flex justify-between border-none !bg-[#1C1C20] sm:!bg-inherit">
           <span className="w-1/2 !text-lg !text-[#F2F2F2] font-bold !bg-[#1C1C20] sm:!bg-inherit">
             {ORDER_DETAILS}
@@ -161,7 +161,7 @@ const OrderDetails: React.FC = () => {
             {PROMOTIONS}
           </span>
         </div>
-        <div className="flex flex-col border-t-0 sm:border-t border-solid border-[#383F47] sm:grid sm:gap-y-0 sm:grid-cols-2 ">
+        <div className="flexColWrapper sm:gap-y-0 sm:grid-cols-2 ">
           <div className="flexWrapper justify-start bg-[#30343B] rounded-t-md">
             <span className="w-auto sm:w-1/5 flex items-center">
               {ORDER}
@@ -185,7 +185,7 @@ const OrderDetails: React.FC = () => {
             </span>
           </div>
         </div>
-        <div className="flex flex-col border-t-0 sm:border-t border-solid border-[#383F47] sm:grid sm:gap-y-0 sm:grid-cols-2 bg-[#30343B]">
+        <div className="flexColWrapper sm:gap-y-0 sm:grid-cols-2 bg-[#30343B]">
           <div className="flexWrapper">
             <span className="w-auto sm:w-1/5 font-light">{SUBMITTED}</span>
             <span className="w-auto sm:w-4/5 font-medium">
@@ -199,14 +199,14 @@ const OrderDetails: React.FC = () => {
             </span>
           </div>
         </div>
-        <div className="flex flex-col border-t-0 sm:border-t border-solid border-[#383F47] sm:grid sm:gap-y-0 sm:grid-cols-2 bg-[#30343B] rounded-b-md">
+        <div className="flexColWrapper sm:gap-y-0 sm:grid-cols-2 bg-[#30343B] rounded-b-md">
           <div className="flexWrapper">
             <span className="w-auto sm:w-1/5 font-light">{LOCALE}</span>
             <span className="w-auto sm:w-4/5 font-medium">
               {orderData?.locale}
             </span>
           </div>
-          <div className="flex justify-between w-full py-1 px-4 sm:p-0 border-none">
+          <div className="flexBlockWrapper py-1 px-4 sm:p-0 border-none">
             <span className="w-auto sm:w-1/5">{ORDER_TYPE}</span>
             <span className="w-auto sm:w-4/5 font-medium">
               {orderData?.customerInfo?.biTier}
@@ -216,17 +216,17 @@ const OrderDetails: React.FC = () => {
       </div>
 
       <div className="flex flex-col-reverse p-0 sm:grid gap-4 sm:grid-cols-2 m-4">
-        <div className="bg-[#1C1C20] sm:bg-[#30343B] p-0 sm:py-4 sm:px-6 grid gap-y-0 grid-cols-1 rounded-md">
+        <div className="bg-[#1C1C20] sm:bg-[#30343B] p-0 sm:py-4 sm:px-6 gridNoGapRounded grid-cols-1">
           <span className="!text-lg font-bold !bg-[#1C1C20] sm:!bg-inherit">
             {STATUS_ACROSS}
           </span>
-          <div className="flex justify-between w-full rounded-t-md border-t-0 py-1 px-4 sm:p-0 sm:border-t border-solid border-[#383F47] bg-[#30343B]">
+          <div className="flexBlockWrapper rounded-t-md border-t-0 py-1 px-4 sm:p-0 sm:border-t border-solid border-[#383F47] bg-[#30343B]">
             <span className="w-auto sm:w-1/6">{ATG}</span>
             <span className="w-auto sm:w-5/6 font-medium">
               {`${orderData?.status} - ${orderData?.sephOrderStatus}`}
             </span>
           </div>
-          <div className="flex justify-between w-full border-t py-1 px-4 sm:p-0 border-solid border-[#383F47] bg-[#30343B]">
+          <div className="flexBlockWrapper border-t filterCardWrapper">
             <span className="w-auto sm:w-1/6 flex items-center">
               {OMS}
               <CustomIcon
@@ -242,16 +242,16 @@ const OrderDetails: React.FC = () => {
               {omsOrderStatus?.orderNumStatus}
             </span>
           </div>
-          <div className="flex justify-between w-full rounded-b-md border-t py-1 px-4 sm:p-0 border-solid border-[#383F47] bg-[#30343B]">
+          <div className="flexBlockWrapper rounded-b-md border-t filterCardWrapper">
             <span className="w-auto sm:w-1/6">{WMS}</span>
             <span className="w-auto sm:w-5/6 font-medium"></span>
           </div>
         </div>
-        <div className="bg-[#1C1C20] sm:bg-[#30343B] p-0 sm:py-4 sm:px-6 grid gap-y-0 grid-cols-1 rounded-md">
+        <div className="bg-[#1C1C20] sm:bg-[#30343B] p-0 sm:py-4 sm:px-6 gridNoGapRounded grid-cols-1">
           <span className="!text-lg font-bold !bg-[#1C1C20] sm:!bg-inherit">
             {CUSTOMER_INFO}
           </span>
-          <div className="flex justify-between w-full border-t-0 sm:border-t py-1 px-4 sm:p-0 border-solid border-[#383F47] bg-[#30343B] rounded-t-md">
+          <div className="flexBlockWrapper border-t-0 sm:border-t filterCardWrapper rounded-t-md">
             <span className="w-auto sm:w-1/6">{NAME}</span>
             <span className="w-auto sm:w-5/6 font-medium">
               {formatName(
@@ -259,13 +259,13 @@ const OrderDetails: React.FC = () => {
               )}
             </span>
           </div>
-          <div className="flex justify-between w-full border-t py-1 px-4 sm:p-0 border-solid border-[#383F47] bg-[#30343B]">
+          <div className="flexBlockWrapper border-t filterCardWrapper">
             <span className="w-auto sm:w-1/6">{EMAIL}</span>
             <span className="w-auto sm:w-5/6 font-medium">
               {orderData?.customerInfo?.email}
             </span>
           </div>
-          <div className="flex justify-between w-full border-t py-1 px-4 sm:p-0 border-solid border-[#383F47] bg-[#30343B] rounded-b-md">
+          <div className="flexBlockWrapper border-t filterCardWrapper rounded-b-md">
             <span className="w-auto sm:w-1/6">{BITIER}</span>
             <span className="w-auto sm:w-5/6 font-medium">
               {orderData?.customerInfo?.biTier}
@@ -274,7 +274,7 @@ const OrderDetails: React.FC = () => {
         </div>
       </div>
 
-      <div className="grid gap-y-0 grid-cols-1 m-4 bg-[#1C1C20] sm:bg-[#30343B] rounded-md p-0 sm:py-4 sm:px-6">
+      <div className="gridNoGapRounded grid-cols-1 m-4 bg-[#1C1C20] sm:bg-[#30343B] p-0 sm:py-4 sm:px-6">
         <div className="flex justify-between border-none !bg-[#1C1C20] sm:!bg-inherit">
           <span className="w-3/4 sm:w-full !text-lg !text-[#F2F2F2] font-bold !bg-[#1C1C20] sm:!bg-inherit">
             {ITEMS_INFO}
@@ -305,13 +305,13 @@ const OrderDetails: React.FC = () => {
         </div>
       </div>
 
-      <div className="grid gap-y-0 m-4 p-0 sm:p-4 sm:bg-[#30343B] rounded-md sm:py-4 sm:px-6">
+      <div className="gridNoGapRounded m-4 p-0 sm:p-4 sm:bg-[#30343B] sm:py-4 sm:px-6">
         <div className="!bg-[#1C1C20] sm:!bg-inherit">
           <span className="block w-[100%] !text-lg font-bold !bg-[#1C1C20] sm:!bg-inherit">
             {PAYMENT_INFO}
           </span>
         </div>
-        <div className="flex flex-col border-t-0 sm:border-t border-solid border-[#383F47] sm:grid gap-y-0 grid-cols-2">
+        <div className="flexColWrapper gap-y-0 grid-cols-2">
           <div className="flexWrapper justify-start bg-[#30343B] rounded-t-md">
             <span className="w-auto sm:w-1/5">{PAYMENT_TYPE}</span>
             <span className="w-auto sm:w-4/5 font-medium">
@@ -325,7 +325,7 @@ const OrderDetails: React.FC = () => {
             </span>
           </div>
         </div>
-        <div className="flex flex-col border-t-0 sm:border-t border-solid border-[#383F47] sm:grid gap-y-0 grid-cols-2">
+        <div className="flexColWrapper gap-y-0 grid-cols-2">
           <div className="flexWrapper bg-[#30343B] rounded-b-md border-none">
             <span className="w-auto sm:w-1/5">{STATUS}</span>
             <span className="w-auto sm:w-4/5 font-medium">
