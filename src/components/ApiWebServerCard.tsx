18d67fab0e5c021e98d0d2d7048caa21868404ab
K.Ajish <k.ajish@litmus7.com>
2023-12-13 13:30:23 +0530

    Reviewed

--- src/components/ApiWebServerCard.tsx
+++ src/components/ApiWebServerCard.tsx
@@ -3,7 +3,6 @@ import { OverlayPanel } from "primereact/overlaypanel";
 import CustomOverlayFocusRoom from "./common/OverlayFocusRoom";
 import { ApiWebServerCard } from "../@types/components/commonTypes";
 
-
 const WebServerCard: React.FC<ApiWebServerCard> = (props) => {
   const op = useRef<OverlayPanel>(null);
   const renderPerTotal = () => {
@@ -23,18 +22,14 @@ const WebServerCard: React.FC<ApiWebServerCard> = (props) => {
   return (
     <>
       <div
-        className={`mt-1.5 flex flex-row max-w-[198px] h-9 border-2 border-gray-108 text-white-100 rounded-xl cursor-pointer font-helvetica
-          !bg-black-101 
+        className={`mt-1.5 flex flex-row max-w-[198px] h-9 border-2 border-gray-108 text-white-100 rounded-xl cursor-pointer font-helvetica !bg-black-101 
          text-sm text-white-500`}
-        onClick={(e) => {op.current?.toggle(e);
-        
+        onClick={(e) => {
+          op.current?.toggle(e);
         }}
       >
         <div className="pl-3 mt-0.5 w-65pr h-9">
-          <div
-            className="w-20 text-10 text-ellipsis overflow-x-hidden whitespace-nowrap uppercase"
-            {...(props.showToolTip ? { title: props.title } : {})}
-          >
+          <div className="w-20 text-10 text-ellipsis overflow-x-hidden whitespace-nowrap uppercase">
             {props.title}
           </div>
           <span className=" relative bottom-2.5 max-w-31 max-h-1.5 text-[8px]">
@@ -43,8 +38,7 @@ const WebServerCard: React.FC<ApiWebServerCard> = (props) => {
         </div>
         <div className="m-auto mr-5">{renderPerTotal()}</div>
       </div>
-       {props.showToolTip && <CustomOverlayFocusRoom {...props}  ref={op} />}
-     
+      {props.showToolTip && <CustomOverlayFocusRoom {...props} ref={op} />}
     </>
   );
 };
