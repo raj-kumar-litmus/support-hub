5a6df345db8b2c85f495d712c52d991559a088b3
Thasnim <Thasnim@litmus7.com>
2024-01-10 13:44:48 +0530

    review comment addressed

--- src/components/NoInternetConnection.tsx
+++ src/components/NoInternetConnection.tsx
@@ -8,15 +8,13 @@ const NoInternetConnection = (props: any) => {
   window.addEventListener("online", () => setOnline(true));
   window.addEventListener("offline", () => setOnline(false));
 
-  if (isOnline) {
-    return props.children;
-  } else {
-    return (
-      <div className="flex h-[100vh] items-center justify-center bg-black-100 text-white-500">
-        No Internet Connection. Please try again later.
-      </div>
-    );
-  }
+  return isOnline ? (
+    props.children
+  ) : (
+    <div className="flex h-[100vh] items-center justify-center bg-black-100 text-white-500">
+      No Internet Connection. Please try again later.
+    </div>
+  );
 };
 
 export default NoInternetConnection;
