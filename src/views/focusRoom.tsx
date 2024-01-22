00a27320eb2030a3914249d6d8ea18c8eda84ef0
raj-kumar-litmus <132570273+raj-kumar-litmus@users.noreply.github.com>
2024-01-22 17:13:18 +0530

    [fix] Remove Duplicate Widgets

--- src/views/focusRoom.tsx
+++ src/views/focusRoom.tsx
@@ -61,30 +61,15 @@ const FocusRoom = () => {
               <div className="h-11h">
                 <KafkaWidget />
               </div>
+              <div className="h-[8.6vh]">
+                <SkuInventoryWidget />
+              </div>
               <div className="grid grid-cols-2 gap-y-1.5h gap-x-0.6w h-[13.1vh]">
                 <ApiWidget />
                 <UfeWidget />
               </div>
               <div className="h-11h">
                 <DatabaseWidget />
-              </div>
-              <div className="space-y-[1.5vh]">
-                <div className="h-[11vh]">
-                  <CancellationWidget />
-                </div>
-                <div className="h-[11vh]">
-                  <KafkaWidget />
-                </div>
-                <div className="grid grid-cols-2 gap-y-[1.5vh] gap-x-[0.6vw] h-[13.1vh]">
-                  <ApiWidget />
-                  <UfeWidget />
-                </div>
-                <div className="h-[11vh]">
-                  <DatabaseWidget />
-                </div>
-                <div className="h-[8.6vh]">
-                  <SkuInventoryWidget />
-                </div>
               </div>
             </div>
           </div>
